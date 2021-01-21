55111787a1ab222e72fc9c7c299bbebfcedeaa64
leeanh <anhldhe130082@fpt.edu.vn>
2021-01-21 21:19:08 +0700

    Finish implemet evaluation

--- core/hook/comem_hooks.py
+++ core/hook/comem_hooks.py
@@ -104,6 +104,53 @@ class COMEMAEEvaluateHook(EvaluateHook):
                 gtFlow_vis, gtFlow = flow_batch_estimate(self.trainer.flownet, input_test, self.trainer.normalize.param['val'],
                                                          output_format=self.trainer.config.DATASET.optical_format, optical_size=self.trainer.config.DATASET.optical_size)
 
+                # Run model
+                motion_output, _, _, _, motion_softmax_score_query, motion_softmax_score_memory, \
+                motion_separateness_loss, motion_compactness_loss , app_output, _, _, _, \
+                app_softmax_score_query, app_softmax_score_memory, app_separateness_loss, \
+                app_compactness_loss = self.trainer.CoMemAE(input_test, train=False)
+
+                test_psnr = psnr_error(app_output, future_test)
+                score, _, _ = amc_score(future_test, app_output, gtFlow, motion_output, wf, wi)
+                test_psnr = test_psnr.tolist()
+                score = score.tolist()
+                psnrs[test_counter+frame_num-1] = test_psnr
+                scores[test_counter+frame_num-1] = score
+                test_counter += 1
+
+                if sn == random_video_sn and (frame_sn in vis_range):
+                    temp = vis_optical_flow(motion_output.detach(), output_format=self.trainer.config.DATASET.optical_format, output_size=(motion_output.shape[-2], motion_output.shape[-1]),
+                                            normalize=self.trainer.normalize.param['val'])
+                    vis_objects = OrderedDict({
+                        'comem_eval_frame': future_test.detach(),
+                        'comem_eval_frame_hat': app_output.detach(),
+                        'comem_eval_flow': gtFlow_vis.detach(),
+                        'comem_eval_flow_hat': temp
+                    })
+                    tensorboard_vis_images(vis_objects, tb_writer, global_steps, normalize=self.trainer.normalize.param['val'])
+
+                if test_counter >= test_iters:
+                    psnrs[:frame_num-1]=psnrs[frame_num-1]
+                    # import ipdb; ipdb.set_trace()
+                    scores[:frame_num-1]=(scores[frame_num-1],) # fix the bug: TypeError: can only assign an iterable
+                    smax = max(scores)
+                    normal_scores = np.array([np.divide(s, smax) for s in scores])
+                    normal_scores = np.clip(normal_scores, 0, None)
+                    psnr_records.append(psnrs)
+                    score_records.append(normal_scores)
+                    print(f'finish test video set {video_name}')
+                    break
+
+        self.trainer.pkl_path = save_results(self.trainer.config, self.trainer.logger, verbose=self.trainer.verbose, config_name=self.trainer.config_name, current_step=current_step, time_stamp=self.trainer.kwargs["time_stamp"],score=score_records, psnr=psnr_records)
+        results = self.trainer.evaluate_function(self.trainer.pkl_path, self.trainer.logger, self.trainer.config, self.trainer.config.DATASET.score_type)
+        self.trainer.logger.info(results)
+        tb_writer.add_text('CoMemAE: AUC of ROC curve', f'auc is {results.auc}',global_steps)
+        return results.auc
 
 
-
+def get_comem_hooks(name):
+    if name in HOOKS:
+        t = eval(name)()
+    else:
+        raise Exception('The hook is not in amc_hooks')
+    return t
